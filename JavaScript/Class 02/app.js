172f6fc49487d78e241345f19dd11776a0f21c95
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-02-22 21:17:54 +0500

    Add files via upload

---
+++ JavaScript/Class 02/app.js
@@ -0,0 +1,159 @@
+// variable decalre
+
+// var number = 80;
+
+// console.log(number);
+
+// variable declare and initialize
+
+// var number = 10;
+
+// var number = 10;
+
+// var number2 = "20";
+
+// console.log(number + number2);
+
+// document.write("<select><option>Faraz</option><option>abc</option></select>");
+
+// var firstName = "abc";
+
+// var number = 10;
+
+// var number2 = 20;
+
+// console.log(number + number2);
+
+// var res = number + 8 + (10 * 16) / 2;
+
+// console.log(res);
+
+// var number;
+
+// number = 20;
+
+// console.log(number);
+
+// var number = 50;
+
+// number = number++;
+
+// console.log(number);
+
+// post increment
+
+// var number = 10;
+
+// var inc = ++number;
+
+// console.log(inc);
+
+// pre increment
+
+// var number = 5;
+
+// var cal = --number + ++number - number++ + number + ++number + ++number + --number - number - ++number - ++number;
+
+// 4 + 5 - 5 + 6
+
+// console.log(cal);
+
+// var firstName = "Mr";
+
+// var lastName = "John";
+
+// var fine = "fine";
+
+// console.log("Hello " + firstName + " " + lastName + "!!. hope you are " + fine);
+
+// console.log("Hello Mr john!!. hope you are fine...");
+
+// var firstName = "Mr";
+
+// var lastName = "Johnson";
+
+// var qualification = "BSCS";
+
+// var section = "A";
+
+// var RollNo = 13213;
+
+// document.write(
+//   "Hello " +
+//     firstName +
+//     " " +
+//     lastName +
+//     "My section is: " +
+//     section +
+//     ". My roll no is: " +
+//     RollNo +
+//     ". mY qaulification is: " +
+//     qualification
+// );
+
+// var number = 5;
+
+// document.write(
+//   number + "x 1 = " + "<b>"+number * 1+"</b>" + "<br>" + number + "x 2 = " + number * 2 + "<br>"
+// );
+
+// document.write("5 x 1 = 5");
+
+// alert("Hello");
+
+// var number1 = prompt("enter first number");
+
+// var number2 = prompt("enter second number");
+
+// console.log(number1 + number2);
+
+// document.write("<h1>" + userInput + "</h1>");
+
+// +
+// Number()
+// parseInt()
+
+var number = +prompt("Enter table number..");
+
+document.write(
+  number +
+    "x 1 =" +
+    number * 1 +
+    "<br>" +
+    number +
+    "x 2 =" +
+    number * 2 +
+    "<br>" +
+    number +
+    "x 3 =" +
+    number * 3 +
+    "<br>" +
+    number +
+    "x 4 =" +
+    number * 4 +
+    "<br>" +
+    number +
+    "x 5 =" +
+    number * 5 +
+    "<br>" +
+    number +
+    "x 6 =" +
+    number * 6 +
+    "<br>" +
+    number +
+    "x 7 =" +
+    number * 7 +
+    "<br>" +
+    number +
+    "x 8 =" +
+    number * 8 +
+    "<br>" +
+    number +
+    "x 9 =" +
+    number * 9 +
+    "<br>" +
+    number +
+    "x 10 =" +
+    number * 10 +
+    "<br>"
+);
