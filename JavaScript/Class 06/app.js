991254ae9a7464be97a6a1495c740ebe06616be0
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-04-02 19:21:19 +0500

    Update app.js

--- JavaScript/Class 06/app.js
+++ JavaScript/Class 06/app.js
@@ -1,7 +1,7 @@
 var Newyeardate = new Date("1 january 2025").getTime();
               
 var currentDate = new Date().getTime();   
-   
+      
 var diff = Newyeardate - currentDate;   
 
 var days = diff / (1000 * 60 * 60 * 24);  
