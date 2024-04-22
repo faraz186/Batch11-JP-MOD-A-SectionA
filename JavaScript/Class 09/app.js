6eb4be8bba4d5e0f3b673d183b2dbd0e1eb1383f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-04-23 00:35:28 +0500

    Update app.js

--- JavaScript/Class 09/app.js
+++ JavaScript/Class 09/app.js
@@ -4,10 +4,10 @@ function makeInvisible() {
   console.log(imageElement);
 }
 
-// function over(e) {
-//   e.src =
-//     "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/2020-Chevrolet-Corvette-Stingray/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=960";
-// }
+function over(e) {
+  e.src =
+    "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/2020-Chevrolet-Corvette-Stingray/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=960";
+}
 
 // function out(e) {
 //   e.src =
