0ea705fa33eeab5bd1fe2fbfb02752605f2aabfa
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-04-24 21:02:50 +0500

    DOM complete..

---
+++ JavaScript/Class 10/app.js
@@ -0,0 +1,40 @@
+// var heading = document.getElementById("heading");
+var main = document.getElementById("main");
+// var link = document.getElementById("link");
+
+// console.log(main.attributes[0].value);
+
+// link.setAttribute("href", "https://www.google.com");
+
+// link.href = "https://www.google.com";
+
+var paraElement = document.createElement("p");
+
+var paraText = document.createTextNode("hello world");
+
+paraElement.appendChild(paraText);
+
+console.log(paraElement);
+
+var main = document.getElementById("main");
+
+main.appendChild(paraElement);
+
+var anchorElement = document.createElement("a");
+
+var anchorText = document.createTextNode("google");
+
+anchorElement.appendChild(anchorText);
+
+anchorElement.setAttribute("href", "https://www.google.com");
+
+main.appendChild(anchorElement);
+
+var divElement = document.createElement("div");
+
+divElement.setAttribute("id", "main");
+
+console.log(divElement);
+
+// paraElement.innerHTML = paraText;
+// console.log(paraText);
