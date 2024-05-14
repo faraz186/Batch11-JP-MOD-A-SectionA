acf9d331a5bc8118b01c9612aa6fe60bbe6785c9
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-05-15 00:36:21 +0500

    Update app.js

--- JavaScript/Class 17/app.js
+++ JavaScript/Class 17/app.js
@@ -1,7 +1,4 @@
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-  
-// Your web app's Firebase configuration
+
 var firebaseConfig = {
   apiKey: "**************************************",
   authDomain: "***************************",
